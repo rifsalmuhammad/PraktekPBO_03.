a29f7de7ce766b0e7a46186eb55293bb2f42a287
rifsalmuhammad <rifsalmuhammad@users.noreply.github.com>
2023-05-29 00:18:33 +0800

    Menggunakan Method Set

--- src/praktekpbo_03/SepedaAksi.java
+++ src/praktekpbo_03/SepedaAksi.java
@@ -3,9 +3,10 @@ package praktekpbo_03;
 public class SepedaAksi {
     public static void main(String[] args) {
         Sepeda s = new Sepeda();
-        s.merk="Kupu-Kupu";
-        s.warna="Merah";
-        s.harga=1000;
+        
+        s.setMerk("Kupu-kupu");
+        s.setWarna("Merah");
+        s.setHarga(1000);
         
         s.cetakInfo();
         
