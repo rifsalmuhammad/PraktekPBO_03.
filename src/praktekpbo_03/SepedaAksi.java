4974d3da055d8359584f454b3e671beb6c4f9296
rifsalmuhammad <rifsalmuhammad@users.noreply.github.com>
2023-05-29 00:12:15 +0800

    Instance, Buat Atribut dan Panggil Method

--- src/praktekpbo_03/SepedaAksi.java
+++ src/praktekpbo_03/SepedaAksi.java
@@ -2,6 +2,12 @@ package praktekpbo_03;
 
 public class SepedaAksi {
     public static void main(String[] args) {
+        Sepeda s = new Sepeda();
+        s.merk="Kupu-Kupu";
+        s.warna="Merah";
+        s.harga=1000;
+        
+        s.cetakInfo();
         
     }
 }
