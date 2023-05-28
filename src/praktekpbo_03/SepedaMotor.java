f7678a9fce7e483a8009ebe85a3b8a679f534dea
rifsalmuhammad <rifsalmuhammad@users.noreply.github.com>
2023-05-29 00:26:59 +0800

    atribut dan method

--- src/praktekpbo_03/SepedaMotor.java
+++ src/praktekpbo_03/SepedaMotor.java
@@ -1,5 +1,10 @@
 package praktekpbo_03;
 
 public class SepedaMotor extends Sepeda {
+    private String JenisMotor;
+    
+    void throttle() {
+        System.out.println("SIIIIUUUUUU.......");
+    }
     
 }
