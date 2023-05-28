2f31421816c425bcdc74829e661675a729414596
rifsalmuhammad <rifsalmuhammad@users.noreply.github.com>
2023-05-29 00:13:19 +0800

    Set Atribut menjadi private

--- src/praktekpbo_03/Sepeda.java
+++ src/praktekpbo_03/Sepeda.java
@@ -2,9 +2,9 @@
 package praktekpbo_03;
 
 public class Sepeda {
-     String merk;
-     String warna;
-     double harga;
+     private String merk;
+     private String warna;
+     private double harga;
      
      
      void cetakInfo(){
